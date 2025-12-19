bcc174aae66f1a2ef6ddec4ad03e2a45bc649ac0
IsabellaDev <129188828+Gregory704@users.noreply.github.com>
2025-12-19 22:59:34 +0100

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,1 +1,10 @@
 UWorld = 0x175C9368
+GNAMES = 0x17483F80
+GEngine = 0x175CAC68
+ProcessEvent 0x949CA | Index = 0x98
+StaticFindObject 0x5E4332
+NamePrivate = 0x20
+CameraLocation = 0x178
+CameraRotation = 0x188
+ClassPrivate = 0x18
+OuterPrivate = 0x8
